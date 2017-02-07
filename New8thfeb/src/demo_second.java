6bebd9fbd2a10a36199420d6a2fe30151a3cc36e
HP <HP@admin>
2017-02-08 02:52:15 +0530

    I don t know!

--- New8thfeb/src/demo_second.java
+++ New8thfeb/src/demo_second.java
@@ -1,4 +1,13 @@
+import java.util.Scanner;
 
 public class demo_second {
+public static void main(String args[])
+{
+	Scanner sc= new Scanner(System.in);
+	System.out.println("Enter the number");
 
+	int n=sc.nextInt();
+	System.out.println("Hello Mr. Pushap!!");
+	
+}
 }
