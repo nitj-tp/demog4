a73cf232bc2949c62cb3529a783b7acdf4fe0fde
HP <HP@admin>
2017-02-08 02:59:28 +0530

    New file and class has been added that take sthe input..!!1

--- New8thfeb/src/demo_class.java
+++ New8thfeb/src/demo_class.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 
 public class demo_class {
 public static void main(String args[])
@@ -5,9 +6,20 @@ public static void main(String args[])
 	System.out.println("Hello World!");
 	System.out.println("HIIIIIIIIIIIIIIII");
 	System.out.println("Hello World!");
+	Scanner sc= new Scanner(System.in);
+	System.out.println("Enter the number");
+
+	int n=sc.nextInt();
+	System.out.println("Hello Mr. Pushap!!");
+	
+
 	System.out.println("HIIIIIIIIIIIIIIII");
 	System.out.println("Hello World!");
 	System.out.println("HIIIIIIIIIIIIIIII");
+	System.out.println("Hello World!");
+	System.out.println("HIIIIIIIIIIIIIIII");
+	System.out.println("Hello World!");
+	
 	
 }
 }
